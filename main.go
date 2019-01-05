b7d9c4307d58427c16d420b1f2da3ea8dddf4180
TomSavas <tomas.savickas@protonmail.com>
2019-01-05 22:55:40 +0000

    Add http serving

--- main.go
+++ main.go
@@ -5,30 +5,39 @@ import (
     "net/http"
     "flag"
 	"github.com/gorilla/websocket"
+	"github.com/gorilla/mux"
 )
 
 var addr = flag.String("addr", "localhost:8080", "http service address")
+
 var upgrader = websocket.Upgrader{} // use default options
 
 func main() {
-    ConnectToDatabase()
+    //For testing purposes ignore cross-site request forgery issues
+    upgrader.CheckOrigin = func (r *http.Request) bool {
+        return true
+    }
 
-    //router.HandleFunc("/global/top/{amountOfTopPlayers}", GetTopPlayers).Methods("GET")
+    router := mux.NewRouter()
+
+    ConnectToDatabase()
+    router.HandleFunc("/global/top/{amountOfTopPlayers}", GetTopPlayers).Methods("GET")
     //router.HandleFunc("/global/position/{playerID}", GetPosition).Methods("GET")
     //router.HandleFunc("/global/save_score", PutScore).Methods("PUT")
-
-    //router.HandleFunc("/room/create", CreateRoom).Methods("GET")
-    //router.HandleFunc("/room/connect/{roomID}", ConnectToRoom).Methods("POST")
-    //router.HandleFunc("/room/disconnect/{roomID}", DisconnectFromRoom).Methods("POST")
-    //router.HandleFunc("/room/{roomID}", GetRoomInfo).Methods("GET")
-
-    //router.HandleFunc("/room/create", CreateRoom)
-
 
     http.HandleFunc("/room/create", CreateRoom)
     http.HandleFunc("/room/observe", Observe)
     http.HandleFunc("/room/join", JoinRoom)
 
 
-    log.Fatal(http.ListenAndServe(*addr, nil))
+    srv := &http.Server{
+        Handler:      router,
+        Addr:         "localhost:8081",//addr2.String(),
+        // Good practice: enforce timeouts for servers you create!
+        // WriteTimeout: 15 * time.Second,
+        // ReadTimeout:  15 * time.Second,
+    }
+
+    go func() {log.Fatal(http.ListenAndServe(*addr, nil))} ()
+    log.Fatal(srv.ListenAndServe())
 }
