d50e2f5898ab2d54c32678ee779e50ce0f1c9184
TomSavas <tomas.savickas@protonmail.com>
2019-02-03 15:31:53 +0000

    Cleanup entrypoint

--- main.go
+++ main.go
@@ -1,43 +1,16 @@
 package main
 
 import (
-    "log"
     "net/http"
-    "flag"
-	"github.com/gorilla/websocket"
-	"github.com/gorilla/mux"
 )
 
-var addr = flag.String("addr", "localhost:8080", "http service address")
-
-var upgrader = websocket.Upgrader{} // use default options
-
 func main() {
-    //For testing purposes ignore cross-site request forgery issues
-    upgrader.CheckOrigin = func (r *http.Request) bool {
-        return true
-    }
-
-    router := mux.NewRouter()
-
-    ConnectToDatabase()
-    router.HandleFunc("/global/top/{amountOfTopPlayers}", GetTopPlayers).Methods("GET")
-    //router.HandleFunc("/global/position/{playerID}", GetPosition).Methods("GET")
-    //router.HandleFunc("/global/save_score", PutScore).Methods("PUT")
+    err := ConnectToDatabase()
+    Fatal(err, "Failed connecting to the database")
 
     http.HandleFunc("/room/create", CreateRoom)
     http.HandleFunc("/room/observe", Observe)
     http.HandleFunc("/room/join", Join)
 
-
-    srv := &http.Server{
-        Handler:      router,
-        Addr:         "localhost:8081",//addr2.String(),
-        // Good practice: enforce timeouts for servers you create!
-        // WriteTimeout: 15 * time.Second,
-        // ReadTimeout:  15 * time.Second,
-    }
-
-    go func() {log.Fatal(http.ListenAndServe(*addr, nil))} ()
-    log.Fatal(srv.ListenAndServe())
+    Fatal(http.ListenAndServe("0.0.0.0:8080", nil), "Failed running a webserver")
 }
