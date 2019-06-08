ea5c946d12197951926c37b082d887837de3d3bb
TomSavas <tomas.savickas@protonmail.com>
2019-06-08 21:00:17 +0100

    Add periodical room removal

--- main.go
+++ main.go
@@ -2,10 +2,20 @@ package main
 
 import (
     "net/http"
+    "time"
 )
 
 func main() {
     err := ConnectToDatabase()
+
+    go func() {
+        for {
+            LogInfo("RoomDB", "Periodically removing empty rooms.")
+            RemoveEmptyRooms()
+            time.Sleep(3600 * 1000 * time.Millisecond)
+        }
+    }()
+
     Fatal(err, "Failed connecting to the database")
 
     http.HandleFunc("/room/create", CreateRoom)
--- room.go
+++ room.go
@@ -28,6 +28,7 @@ type Room struct {
     PlayerIDs    []string  `json:"playerids"`
     ObserverIDs  []string  `json:"observerids"`
     OwnerID      string    `json:"ownerid"`
+    CreationTime string    `json:"creationtime"`
 }
 
 type RoomInfo struct {
@@ -46,7 +47,7 @@ func NewRoom(ownerID string, creatorIsPlayer bool) *Room {
         observers = []string{ownerID}
     }
 
-    return &Room{GenerateUniqueRoomCode(GetRooms()), WaitingForPlayers, players, observers, ownerID}
+    return &Room{GenerateUniqueRoomCode(GetRooms()), WaitingForPlayers, players, observers, ownerID, time.Now().UTC().String()}
 }
 
 func GetExistingRoom(roomID string) *Room {
@@ -61,10 +62,56 @@ func GetRooms() *mgo.Collection {
     return GetCollection("Rooms")
 }
 
+func RemoveEmptyRooms() {
+    rooms := GetRooms()
+
+    LogInfo("RoomDB", "Periodically removing rooms with empty playerids.")
+    changeInfo, err := rooms.RemoveAll(bson.M{"playerids": bson.M{"$eq": []string{} }})
+    changeInfoString := "Matched: " + string(changeInfo.Matched) + " removed: " + string(changeInfo.Removed)
+    if err != nil {
+        Log(err, "RoomDB", "Failed periodically removing rooms with empty playerids." + changeInfoString)
+    } else {
+        Log(err, "RoomDB", changeInfoString)
+    }
+
+    LogInfo("RoomDB", "Periodically removing rooms that have been created two hours ago.")
+    changeInfo, err = rooms.RemoveAll(bson.M{"creationtime": bson.M{"$lte": time.Now().UTC().String() }})
+    changeInfoString = "Matched: " + string(changeInfo.Matched) + " removed: " + string(changeInfo.Removed)
+    if err != nil {
+        Log(err, "RoomDB", "Failed periodically removing rooms that have been created two hours ago." + changeInfoString)
+    } else {
+        Log(err, "RoomDB", changeInfoString)
+    }
+
+    // Remove rooms that have playerids that are offline 
+    LogInfo("RoomDB", "Periodically removing rooms that have only offline players in playerids.")
+    var potentiallyOfflineRooms []Room
+    err = rooms.Find(bson.M{}).Limit(10000).All(&potentiallyOfflineRooms)
+    Log(err, "RoomDB", "Failed periodically removing rooms that have only offline players in playerids.")
+
+    if err != nil {
+        return
+    }
+
+    for _, room := range potentiallyOfflineRooms {
+        hasAnyOnlinePlayers := false
+
+        for _, playerID := range room.PlayerIDs {
+            player, _ := GetExistingPlayer(playerID)
+            hasAnyOnlinePlayers = hasAnyOnlinePlayers || player.Online
+        }
+
+        if hasAnyOnlinePlayers {
+            err = rooms.Remove(bson.M{"id": room.ID})
+            Log(err, "RoomDB", "Failed periodical remove of room " + room.ID + ".")
+        }
+    }
+}
+
 // Returns a value representing whether the room was upated
 func (room *Room) ReadWithStatusReport() bool {
     //Bug - copies slice pointers
-    oldRoom := &Room{room.ID, room.State, room.PlayerIDs, room.ObserverIDs, room.OwnerID}
+    oldRoom := &Room{room.ID, room.State, room.PlayerIDs, room.ObserverIDs, room.OwnerID, room.CreationTime}
     room.Read()
 
     return !room.Equals(oldRoom)
